9b331ed119a64baaf4f52f3be15d56b173e8e654
Kashish Rahate <114670210+KashishRahate@users.noreply.github.com>
2024-05-14 02:35:45 +0530

    Add files via upload

---
+++ api/routes/booking.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const { isLoggedIn } = require("../middlewares/user");
+
+const {
+  createBookings,
+  getBookings,
+  deleteBookings,
+} = require("../controllers/bookingController");
+
+// Protected routes (user must be logged in)
+router
+  .route("/")
+  .get(isLoggedIn, getBookings)
+  .post(isLoggedIn, createBookings)
+  .delete(deleteBookings);
+
+module.exports = router;
